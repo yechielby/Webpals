bd8a565a88b0eab1495c436cd9bc93467e04eaec
yechiel.by <yechiel.by@gmail.com>
2019-09-18 21:48:47 +0300

    Created Model -a,  and added vendor folder

---
+++ api/database/factories/GradeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Grade;
+use Faker\Generator as Faker;
+
+$factory->define(Grade::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
