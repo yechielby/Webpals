6a31490344852bc6723cba19cb21d06b12fa64db
yechielby <yechiel.by@gmail.com>
2019-09-19 13:28:19 +0300

    Relationship created between Student and Grade

--- api/app/Model/Grade.php
+++ api/app/Model/Grade.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+use App\Model\Student;
 use Illuminate\Database\Eloquent\Model;
 
 class Grade extends Model
 {
-    //
+    public function student(){
+        
+        return $this->belongsTo(Student::class);
+
+    }
 }
--- api/app/Model/Student.php
+++ api/app/Model/Student.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+use App\Model\Grade;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public function grades(){
+        
+        return $this->hasMany(Grade::class);
+
+    }
 }
